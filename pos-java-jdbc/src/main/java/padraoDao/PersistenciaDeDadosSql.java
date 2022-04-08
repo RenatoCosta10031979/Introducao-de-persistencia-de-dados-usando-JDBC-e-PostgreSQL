d611d3b5f830af07094999f6d0cecd9e67d8d01c
RenatoCosta10031979 <jsrenatoguccing@gmail.com>
2022-04-08 12:18:28 -0300

    criando método atualizar(update) registros tabela sql

--- pos-java-jdbc/src/main/java/padraoDao/PersistenciaDeDadosSql.java
+++ pos-java-jdbc/src/main/java/padraoDao/PersistenciaDeDadosSql.java
@@ -3,6 +3,7 @@ package padraoDao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +19,8 @@ public class PersistenciaDeDadosSql {
 
 		connection = SingleConnection.getConnection();
 	}
+	// --------------------MÉTODO INSERIR REGISTRO NA TABELA
+	// ------------------------//
 
 	// Método inserts insere os dados na tabela dadospessoais
 	public void inserirRegistros(DadosPessoas dadosPessoas) {
@@ -45,6 +48,9 @@ public class PersistenciaDeDadosSql {
 
 	}
 
+	// -------MÉTODO CONSULTAR (SELECT), TODOS OS REGISTRO DA TABELA (RETORNA UMA
+	// LISTA)------//
+
 	public List<DadosPessoas> consultarRegistros() throws Exception {
 		List<DadosPessoas> listaDeRegistros = new ArrayList<DadosPessoas>();
 		// Comando sql para consultar os regitros da tabela dadospessoais.
@@ -68,22 +74,114 @@ public class PersistenciaDeDadosSql {
 		return listaDeRegistros;
 	}
 
-	public DadosPessoas retornarRegistros(Long id) throws Exception{
+	// -------MÉTODO CONSULTAR (SELECT), UM REGISTRO POR VEZ DA TABELA (PELA QUERY
+	// ID)------//
+
+	// Método que retorna um único registro ( pela query id), Data table caso ele
+	// exista.
+	public DadosPessoas retornarRegistros(Long id) throws Exception {
 		DadosPessoas retornarUmRegistroDaTabela = new DadosPessoas();
-		// Comando sql para consultar os regitros da tabela dadospessoais.
-		 String sql = "select * from dadospessoais where id = " + id;
-		 
-		 PreparedStatement statement = connection.prepareStatement(sql);
-		 ResultSet resultado = statement.executeQuery();
-		 //O método while executa a leitura de um único registro  da tabela.
-		 while(resultado.next()) {
-			 
-			 retornarUmRegistroDaTabela.setId(resultado.getLong("id"));
-			 retornarUmRegistroDaTabela.setNome(resultado.getString("nome"));
-			 retornarUmRegistroDaTabela.setCpf(resultado.getString("cpf"));
-			 retornarUmRegistroDaTabela.setEmail(resultado.getString("email"));
-		 }
-		 
-		 return retornarUmRegistroDaTabela;
-	}	
+
+		// Executa o comando sql para consultar os regitros da tabela dadospessoais.
+		String sql = "select * from dadospessoais where id = " + id;
+
+		// Recebe os comandos sql e faz os envios das informações
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		/*
+		 * Percorre um data table de alguma consulta ao banco de dados. O ResultSet
+		 * coloca o seu cursor na primeira linha do Data Table, e guarda o resultado de
+		 * uma pesquisa numa estrutura de dados que pode ser percorrida de forma que
+		 * você possa ler os dados do banco.
+		 */
+		ResultSet resultado = statement.executeQuery();
+
+		/*
+		 * O método while executa a leitura de um único registro da tabela. O método
+		 * next(), permite que o ponteiro seja direcionado para próxima linha da tabela
+		 * caso exista.
+		 */
+		while (resultado.next()) {
+
+			retornarUmRegistroDaTabela.setId(resultado.getLong("id"));
+			retornarUmRegistroDaTabela.setNome(resultado.getString("nome"));
+			retornarUmRegistroDaTabela.setCpf(resultado.getString("cpf"));
+			retornarUmRegistroDaTabela.setEmail(resultado.getString("email"));
+		}
+		// Retorna para o usuário o registro Data Table
+		return retornarUmRegistroDaTabela;
+	}
+
+	// -------MÉTODO ALTERAR (UPDATE), OS REGISTROS DA TABELA (USANDO A QUERY
+	// ID)------//
+
+	public void alterar(DadosPessoas dadosPessoas) {
+
+		// <<--------Executa o comando sql para atualizar o nome no registro da tabela ------>
+		String sqlUpdateNome = "update dadospessoais set nome = ? where id = " + dadosPessoas.getId();
+
+		try {
+			// Recebe os comandos sql e prepara o envio das informações
+			PreparedStatement statement = connection.prepareStatement(sqlUpdateNome);
+			statement.setString(1, dadosPessoas.getNome());
+			statement.execute();
+			connection.commit(); // salva o registro no banco de dados
+
+		} catch (Exception e) {
+
+			try {
+				// desfaz a última atualização
+				connection.rollback();
+			} catch (SQLException e1) {
+
+				e1.printStackTrace();
+			}
+
+		}
+		// <<--------Executa o comando sql para atualizar o cpf no registro da tabela ------>
+		String sqlUpdateCpf = "update dadospessoais set cpf = ? where id = " + dadosPessoas.getId();
+		// String sql ="update dadospessoais set email = ? where id = " +
+		// dadosPessoas.getId();
+
+		try {
+			// Recebe os comandos sql e prepara o envio das informações
+			PreparedStatement statement = connection.prepareStatement(sqlUpdateCpf);
+			statement.setString(1, dadosPessoas.getCpf());
+			statement.execute();
+			connection.commit(); // salva o registro no banco de dados
+
+		} catch (Exception e) {
+
+			try {
+				// desfaz a última atualização
+				connection.rollback();
+			} catch (SQLException e1) {
+
+				e1.printStackTrace();
+			}
+
+		}
+		// <<--------Executa o comando sql para atualizar o email no registro da tabela------>
+		String sqlUpdateEmail = "update dadospessoais set email = ? where id = " + dadosPessoas.getId();
+
+		try {
+			// Recebe os comandos sql e prepara o envio das informações
+			PreparedStatement statement = connection.prepareStatement(sqlUpdateEmail);
+			statement.setString(1, dadosPessoas.getEmail());
+			statement.execute();
+			connection.commit(); // salva o registro no banco de dados
+
+		} catch (Exception e) {
+
+			try {
+				// desfaz a última atualização
+				connection.rollback();
+			} catch (SQLException e1) {
+
+				e1.printStackTrace();
+			}
+
+		}
+
+	}
 }
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TestesDePersistencia.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TestesDePersistencia.java
@@ -24,10 +24,13 @@ public class TestesDePersistencia {
 		 * PersistenciaDeDadosSql(); DadosPessoas dadosPessoa = new DadosPessoas();
 		 * persistirDadosSql.inserirRegistros(dadosPessoa);
 		 */
-
+		
+		// <<---EXECUTA O MÉTODO CONSULTAR  (SELECT) REGISTRO TABELA------->>
+		//                 (RETORNA UMA LISTA DE REGISTROS)
+		
 		// Executa o comando sql de consultar e exibir todos os registros gravados na
 		// tabela dadospessoais
-		PersistenciaDeDadosSql persistenciaDeDadosSqlListar = new PersistenciaDeDadosSql();
+		/*PersistenciaDeDadosSql persistenciaDeDadosSqlListar = new PersistenciaDeDadosSql();
 		try {
 
 			List<DadosPessoas> ListaDeRegistros = persistenciaDeDadosSqlListar.consultarRegistros();
@@ -38,10 +41,13 @@ public class TestesDePersistencia {
 			}
 
 		} catch (Exception e) {
-		}
+		}*/
 		
 		
-		PersistenciaDeDadosSql persistenciaDeDadosSqlUmDadoPorVez = new PersistenciaDeDadosSql();
+		// <<------- EXECUTA O MÉTODO CONSULTAR  (SELECT) REGISTRO TABELA------->>
+		//						RETORNA UM REGISTRO POR VEZ USANDO A QUERY ID
+		
+		/*PersistenciaDeDadosSql persistenciaDeDadosSqlUmDadoPorVez = new PersistenciaDeDadosSql();
 		
 		try {
 			
@@ -50,6 +56,27 @@ public class TestesDePersistencia {
 		
 		} catch (Exception e) {
 			
+		}*/
+		
+		// <<------- EXECUTA O MÉTODO ATUALIZAR (UPDATE) REGISTRO TABELA------->>
+		try {
+			PersistenciaDeDadosSql persistenciaDeDadosSql = new PersistenciaDeDadosSql();
+			
+			/* Instância o objeto do tipo DadosPessoas que recebe o método retornarRegistro
+			e passamos o parâmetro o id do registro da tabela para atualizar os dados de registro da tabela.*/
+			DadosPessoas alterarRegistro = persistenciaDeDadosSql.retornarRegistros(3L);
+			
+			alterarRegistro.setNome("Lavínia Gabriela Joana da Paz");
+			alterarRegistro.setCpf("699.962.405-41");
+			alterarRegistro.setEmail("lavinia-dapaz75@alstom.com");
+			
+			/*O objeto persistencidaDeDadosSql acessa ao metodo alterar (update) e passa o 
+			o objeto consultarRegistro como parâmetro que armazena o valor do id passado pelo usuário.*/
+			persistenciaDeDadosSql.alterar(alterarRegistro);
+			
+			JOptionPane.showMessageDialog(null, "Os dados foram atualizados");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
